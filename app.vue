1661f84132e305f4c301882949f0cd661698bea6
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2023-02-06 23:41:45 +0300

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    qwd
+  </div>
+</template>
+
+<script setup>
+// console.log(await $fetch('/api/hook', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     message: 'Good Morning'
+//   })
+// }))
+
+
+</script>
--- server/api/hook.post.ts
+++ server/api/hook.post.ts
@@ -1,35 +1,38 @@
 import { ChatGPTAPI } from 'chatgpt'
-import { Telegraf } from 'telegraf';
+import qs from 'query-string';
 
+const isArabic = (text: string) => {
+    var arabic = /[\u0600-\u06FF]/;
+    return arabic.test(text)
+}
 
 export default defineEventHandler(async (event) => {
+    const SERVER_URL = "https://formal-email-bot.vercel.app/api/hook"
+    const TELEGRAM_API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`
+
     // Message is the email body
-    const { message } = await readBody(event)
+    const { message: { chat: { id }, text } } = await readBody(event)
+
+    const rewriteMessage = isArabic(text) ? "اعد كتابتها بصيغة رسالة بريد رسمية" : "Rewrite in formal Email."
 
     // ChatGPT API
     const api = new ChatGPTAPI({
-        apiKey: process.env.OPENAI_API_KEY
+        apiKey: process.env.OPENAI_API_KEY || '',
     })
 
-    // Telegraf API for Telegram Bot
-    const bot = new Telegraf(process.env.BOT_TOKEN);
-
-
     // Send message to ChatGPT API
-    // const res = await api.sendMessage(message + '\n\n\n Rewrite in formal Email.')
+    const res = await api.sendMessage(text + `\n\n\n ${rewriteMessage}`, {
+        timeoutMs: 2 * 60 * 1000
+    })
     // res.text
 
+    // Format message
+    const messageQuery = qs.stringify({
+        text: res.text
+    })
 
-    bot.on("text", ctx => ctx.reply("Hello"));
-
-
-    // Start webhook via launch method (preferred)
-    bot.launch({
-        webhook: {
-            domain: "localhost",
-            port: 8080,
-        },
-    });
+    // Send message to Telegram
+    await $fetch(`${TELEGRAM_API}/sendMessage?chat_id=${id}&${messageQuery}`, { method: "POST" })
 
     return {
         api: "test"
